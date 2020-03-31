898a894a808ae12797dcca27631758fd8dc79014
GuGaYoung <rkdudda12@naver.com>
2020-03-31 20:11:12 +0900

    Modify Variables

--- src/Farm/Player.java
+++ src/Farm/Player.java
@@ -6,14 +6,14 @@ public class Player {
 	public int energy = 100;
 	public int money = 0;
 	
-	public int numOfPumpkinSeed = 10; //가지고 있는 호박씨의 수
-	public int numOfOnionSeed = 10; //가지고 있는 양파씨의 수 
-	public int numOfCabbageSeed = 10; //가지고 있는 양배추씨의 수
-	public int numOfCarrotSeed = 10; //가지고 있는 당근씨의 수
-	public int numOfPumpkin = 0; //가지고 있는 호박의 수
-	public int numOfOnion = 0; //가지고 있는 양파의 수 
-	public int numOfCabbage = 0; //가지고 있는 양배추의 수
-	public int numOfCarrot = 0; //가지고 있는 당근의 수
+	public int amountPumpkinSeed = 10; //가지고 있는 호박씨의 수
+	public int amountOnionSeed = 10; //가지고 있는 양파씨의 수 
+	public int amountCabbageSeed = 10; //가지고 있는 양배추씨의 수
+	public int amountCarrotSeed = 10; //가지고 있는 당근씨의 수
+	public int amountPumpkin = 0; //가지고 있는 호박의 수
+	public int amountOnion = 0; //가지고 있는 양파의 수 
+	public int amountCabbage = 0; //가지고 있는 양배추의 수
+	public int amountCarrot = 0; //가지고 있는 당근의 수
 	
-	public int numOfBone = 2;//가지고 있는 뼈의 수
+	public int amountBone = 2;//가지고 있는 뼈의 수
 }
